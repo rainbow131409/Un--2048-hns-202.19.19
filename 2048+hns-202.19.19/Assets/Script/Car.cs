dc1d48eb03ede0a18e686fc28cb700f76e92ceb6
rainbow131409 <90996538+rainbow131409@users.noreply.github.com>
2021-10-17 13:05:29 +0800

    建立程式與套用程式

---
+++ 2048+hns-202.19.19/Assets/Script/Car.cs
@@ -0,0 +1,22 @@
+//  單行註解:說明文字，不執行@#
+//  功能  2021.10.17
+//  開發者 趙庭尉 2021.10.17
+using UnityEngine;  //  引用 Unity Engine API
+
+//  Class  類別(藍圖) 名稱要跟檔名相同，大小寫也要相同 
+public class Car : MonoBehaviour
+{
+    // 欄位認識 Field，儲存資料
+    // 語法:
+    // 資料類型 欄位名稱 指定 預設值 結束符號
+    // 常用基本四大資料類型
+    // 1. 類型   沒有小數點的正負值  int    預設值0
+    // 2. 浮點數 有小數點的正負值    float  預設值0
+    // 3. 字串   文字資訊           string 預設值空
+    // 4. 布林值 是與否 true、false bool   預設值false
+    int cc = 2000;
+    float weight = 3.5f;
+    string brand = "賓士";
+    bool hasSkyWindow = true;
+        
+}
