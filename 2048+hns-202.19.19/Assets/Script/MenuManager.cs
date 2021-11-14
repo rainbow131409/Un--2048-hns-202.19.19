46f007a1f9bb8cdf14ee244ffb86af59d447ce6c
rainbow131409 <90996538+rainbow131409@users.noreply.github.com>
2021-11-14 17:16:20 +0800

    怪物設定

--- 2048+hns-202.19.19/Assets/Script/MenuManager.cs
+++ 2048+hns-202.19.19/Assets/Script/MenuManager.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-using UnityEngine.Audio;            // 引用
+using UnityEngine.Audio;            // 引用 音效    命名空間
 using UnityEngine.SceneManagement;  // 引用 場景管理 命名空間
 
 /// <summary>
@@ -17,10 +17,20 @@ public class MenuManager : MonoBehaviour
     /// <summary>
     /// 開始遊戲
     /// </summary>
-    public void StartGame() 
+    public void StartGame(float delay) 
+    {
+        // 使用繼承類別的成員語法:
+        // 繼承類別語法
+        // 方法名稱(對應的引數)
+        // 延遲 delay 秒後呼叫 方法
+        Invoke("DelayStartGame", delay);
+    }
+
+    private void DelayStartGame() 
     {
         // 場景管理.載入場景(場景名稱)
         SceneManager.LoadScene("遊戲場景");
+            
     }
 
     /// <summary>
@@ -42,12 +52,18 @@ public class MenuManager : MonoBehaviour
     /// <summary>
     /// 結束遊戲
     /// </summary>
-    public void QuitGame() 
+    public void QuitGame(float delay) 
+    {
+        Invoke("DelayQuitGame", delay);
+    }
+
+    private void DelayQuitGame() 
     {
         // 應用程式.離開();
         // Quit 不會再 Editor 執行，發布執行黨 手機、PC
         Application.Quit();
 
         print("離開遊戲");
+            
     }
 }
