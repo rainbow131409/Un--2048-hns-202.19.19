8464ba82fac040febedb90f18fbaa4c99c72e462
rainbow131409 <90996538+rainbow131409@users.noreply.github.com>
2021-11-21 17:07:42 +0800

    區塊程式設定(各種)

---
+++ 2048+hns-202.19.19/Assets/Script/Learm2DArray.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+using System.Linq;  // LinQ Query 查詢語言 - 資料查詢
+
+/// <summary>
+/// 認識二維陣列
+/// </summary>
+public class Learm2DArray : MonoBehaviour
+{
+    // 一維陣列
+    public int[] numbers = { 1, 7, 9, 10, 50 };
+    // 二維陣列 : 不會顯示 - 裝套件 Odin 自己寫 Editor
+    public int[,] blocks = { { 2, 4 }, { 6, 8 } };
+
+    public string[,] objects = new string[4, 6];
+
+    public int[,] scores = { { 70, 90 }, { 77, 30 }, { 10, 20 } };
+
+    private void Start()
+    {
+#region 存取
+        // 一維陣列存取
+        numbers[4] = 99;
+        print("一維陣列第五筆資料:" + numbers[4]);
+
+        // 二維陣列存取
+        print("二維陣列第二列第一欄 1, 0 :" + blocks[1, 0]);
+        blocks[1, 0] = 7;
+        print("二維陣列第二列第一欄 1, 0 :" + blocks[1, 0]);
+
+        print("二維陣列第一維度長度:" + scores.GetLength(0));
+        print("二維陣列第二維度長度:" + scores.GetLength(1));
+
+        string result = "";
+        for (int i = 0; i < scores.GetLength(0); i++)
+        {
+            for (int j = 0; j < scores.GetLength(1); j++)
+            {
+                result += scores[i, j] + " | ";
+            }
+
+            result += "\n";
+        }
+        print(result);
+        #endregion
+
+        #region 資料搜尋
+
+        // 搜尋 number 一維陣列內大於等於 10 的資料
+        // var 無類型資料型態
+        // from 資料A in 陣列       -從陣列搜尋資料並保存為 資料A
+        // where 資料A 條件         -判斷 資料A 是否符合條件
+        // select 資料A;           -選取 符合條件的 資料A
+        var numberGratgerTen =
+            from int n in numbers
+            where n >= 10
+            select n;
+
+        print("符合 >= 10 資料有幾筆:" + numberGratgerTen.Count());
+
+        for (int i = 0; i < numberGratgerTen.Count(); i++)
+        {
+            print(">= 10 的資料為 :" + numberGratgerTen.ToArray()[1]);
+        }
+
+        // scores 不及格的分數有哪些
+        var noPass =
+            from int no in scores
+            where no < 60
+            select no;
+
+        for (int i = 0; i < noPass.Count(); i++)
+        {
+            print("不及格分數:" + noPass.ToArray()[i]);
+        }
+        #endregion
+
+    }
+
+
+}
---
+++ 2048+hns-202.19.19/Assets/Script/System2048.cs
@@ -0,0 +1,137 @@
+using UnityEngine;
+using System.Linq;
+
+/// <summary>
+/// 2048 系統
+/// 儲存每個區塊資料
+/// 管理隨機生成
+/// 滑動控制
+/// 數字合併判定
+/// 遊戲機制判定
+/// </summary>
+public class System2048 : MonoBehaviour
+{
+    [Header("空白區塊")]
+    public Transform[] blocksEmpty;
+    [Header("數字區塊")]
+    public GameObject goNumberBlock;
+    [Header("畫布 2048")]
+    public Transform traCanvas2048;
+
+    // 私人欄位顯示在屬性面板上
+    [SerializeField]
+    private Direction direction;
+
+    /// <summary>
+    /// 所有區塊資料
+    /// </summary>
+    public BlockData[,] blocks = new BlockData[4, 4];
+
+    private void Start()
+    {
+        Initialize();
+    }
+
+    /// <summary>
+    /// 初始化資料
+    /// </summary>
+    private void Initialize()
+    {
+        for (int i = 0; i < blocks.GetLength(0); i++)
+        {
+            for (int j = 0; j < blocks.GetLength(1); j++)
+            {
+                blocks[i, j] = new BlockData();
+                blocks[i, j].v2Index = new Vector2Int(i, j);
+                blocks[i, j].v2Position = blocksEmpty[i * blocks.GetLength(1) + j].position;
+            }
+        }
+
+        PrintBlockData();
+        CreateRandomNumberBlock();
+        CreateRandomNumberBlock();
+    }
+
+    private void PrintBlockData() 
+    {
+        string result = "";
+
+        for (int i = 0; i < blocks.GetLength(0); i++)
+        {
+            for (int j = 0; j < blocks.GetLength(1); j++)
+            {
+                // 編號 、數字與座標
+                // 編號、數字與物件
+                result += "編號" + blocks[i, j].v2Index + ")" + "<color=red>數字:" + blocks[i, j].number + "</color> <color=yellow>" + blocks[i,j].v2Position + "</color> |";
+            }
+
+            result += "\n";
+        }
+
+        print(result);
+    }
+
+    /// <summary>
+    /// 建立隨機數字區塊
+    /// 判斷所有區塊內沒有數字的區塊 - 數字為零
+    /// 隨機挑選一個
+    /// 生成數字放到該區塊內
+    /// </summary>
+    private void CreateRandomNumberBlock()
+    {
+        var equalZero =
+            from BlockData data in blocks
+            where data.number == 0
+            select data;
+
+        print("為零的資料有幾筆:" + equalZero.Count());
+
+        int randomIndex = Random.Range(0, equalZero.Count());
+        print("隨機編號:" + randomIndex);
+
+        // 選出隨機區塊 編號
+        BlockData dataRandom = blocks[equalZero.ToArray()[randomIndex].v2Index.x, equalZero.ToArray()[randomIndex].v2Index.y];
+        // 將數字 2 
+        dataRandom.number = 2;
+
+        PrintBlockData();
+
+        // 實例化-生成(物件，父物件)
+        GameObject tempBlock = Instantiate(goNumberBlock, traCanvas2048);
+        // 生成區塊 指定座標
+        tempBlock.transform.position = dataRandom.v2Position;
+        // 儲存 生成區塊 資料
+        dataRandom.goBlock = tempBlock;
+
+        
+    }
+}
+
+/// <summary>
+/// 區塊資料
+/// 每個區塊遊戲物件、座標、編號、數字
+/// </summary>
+public class BlockData
+{
+    /// <summary>
+    /// 區塊內數字物件
+    /// </summary>
+    public GameObject goBlock;
+    /// <summary>
+    /// 區塊座標
+    /// </summary>
+    public Vector2 v2Position;
+    /// <summary>
+    /// 區塊編號:二維陣列內的編號
+    /// </summary>
+    public Vector2Int v2Index;
+    /// <summary>
+    /// 區塊數字:預設為 0 , 或者 2、4、8......2048
+    /// </summary>
+    public int number;
+}
+
+public enum Direction 
+{
+    None, Right, Left, Up, Down
+}
